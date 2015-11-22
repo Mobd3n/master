47d38aa4611735c219c1a040f57c2691156c48df
Mobd3n <pro@ebram.ml>
2015-11-22 23:03:03 +0200

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+//<![CDATA[
+window.onload = function(){
+
+    var copy=document.getElementById("credit");
+    
+    if(copy==null){
+        window.location.href="http://mobd3-network.blogspot.com/";
+    };
+    copy.setAttribute("href","http://mobd3-network.blogspot.com/");
+    copy.innerHTML="مبدع نتورك"
+};
+//]]>
+</script>
